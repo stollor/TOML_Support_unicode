ffeeb522f4825109807be1eeb5320e645d03a288
Stollor <stollor@users.noreply.github.com>
2026-06-22 16:04:40 +0800

    feat: add Chinese character support in Rust lexer for bare keys

--- crates/taplo/src/syntax.rs
+++ crates/taplo/src/syntax.rs
@@ -17,12 +17,12 @@ pub enum SyntaxKind {
     #[regex(r"#[^\n\r]*")]
     COMMENT,
 
-    #[regex(r"[A-Za-z0-9_-]+", priority = 2)]
+    #[regex(r"[A-Za-z0-9_\-\p{CJK}\p{CJK_Unified_Ideographs}\p{CJK_Unified_Ideographs_Extension_A}]+", priority = 2)]
     IDENT,
 
     /// Not part of the regular TOML syntax, only used to allow
     /// glob patterns in keys.
-    #[regex(r"[*?A-Za-z0-9_-]+")]
+    #[regex(r"[*?A-Za-z0-9_\-\p{CJK}\p{CJK_Unified_Ideographs}\p{CJK_Unified_Ideographs_Extension_A}]+")]
     IDENT_WITH_GLOB,
 
     #[token(".")]
